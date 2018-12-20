38ecee83a892d0c558310c2e51c05e06dca0f70a
Viktor Hubert <rpgmorpheus@gmail.com>
2018-12-20 15:55:33 +0100

    Test refactors to allow for ChromeHeadless running in Travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 module.exports = function(config) {
   const singleRun = process.env.KARMA_SINGLE_RUN !== "false";
+  const browsers = (process.env.KARMA_BROWSERS || "Chrome").split(",");
 
   config.set({
     basePath: ".",
@@ -7,43 +8,14 @@ module.exports = function(config) {
     files: ["browser-tests/dist/index.js"],
     autoWatch: true,
 
-    // todo come up with a way to switch in between dev / CI setup
-    browsers: ["Chrome"],
+    browsers,
     reporters: ["spec"],
-
-    // reporters: ["spec", "saucelabs"],
-    // browsers: ["sl_chrome_70"],
 
     singleRun,
 
     // Max concurrency for SauceLabs OS plan
     concurrency: 5,
 
-    // @see https://wiki.saucelabs.com/display/DOCS/Platform+Configurator/
-    customLaunchers: {
-      sl_chrome_35: {
-        base: "SauceLabs",
-        browserName: "chrome",
-        platform: "Windows 7",
-        version: "35"
-      },
-      sl_chrome_70: {
-        base: "SauceLabs",
-        browserName: "chrome",
-        platform: "Windows 10",
-        version: "70"
-      }
-    },
-
-    // Saucelabs launcher
-    // sauceLabs: {
-    //   testName: 'react-container-query',
-    //   public: 'public'
-    // },
-    sauceLabs: {
-      testName: "use-resize-observer",
-      public: "public"
-    },
     client: {
       jasmine: {
         // Order of the tests matter, so don't randomise it
