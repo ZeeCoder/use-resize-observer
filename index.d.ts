8a462bdcaf1f28b24a7fb23e270b2eb5e721e731
Imran Salam <imran@ebi.ac.uk>
2019-02-11 10:48:19 +0000

    Add type definitions

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for use-resize-observer
+// Project: use-resize-observer
+
+import { RefObject } from 'react';
+
+export default function useResizeObserver(): [RefObject<HTMLElement>, number, number];
+
