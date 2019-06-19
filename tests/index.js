f15ad9e916bfb08f62f20280735a78813842f56a
Михайлов Антон <anion155@gmail.com>
2019-06-19 17:06:15 +0500

    Test hook accepted already created reference.

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 import useResizeObserver from "../dist/bundle.esm";
 import delay from "delay";
@@ -9,25 +9,30 @@ import delay from "delay";
 import "babel-regenerator-runtime";
 
 const Observed = () => {
-  const [ref, width, height] = useResizeObserver();
+  const [ref1, width1, height1] = useResizeObserver();
+  const styles = {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    width: "100%",
+    height: "100%",
+    background: "grey",
+    color: "white",
+    fontWeight: "bold"
+  };
+
+  const ref2 = useRef();
+  const [width2, height2] = useResizeObserver(ref2);
 
   return (
-    <div
-      ref={ref}
-      id="observed"
-      style={{
-        position: "absolute",
-        left: 0,
-        top: 0,
-        width: "100%",
-        height: "100%",
-        background: "grey",
-        color: "white",
-        fontWeight: "bold"
-      }}
-    >
-      {width}x{height}
-    </div>
+    <>
+      <div ref={ref1} id="observed1" style={styles}>
+        1 {width1}x{height1}
+      </div>
+      <div ref={ref2} id="observed2" style={styles}>
+        2 {width2}x{height2}
+      </div>
+    </>
   );
 };
 
@@ -41,17 +46,19 @@ beforeAll(() => {
   ReactDOM.render(<Observed />, app);
 
   global.app = app;
-  global.observed = document.querySelector("#observed");
+  global.observed1 = document.querySelector("#observed1");
+  global.observed2 = document.querySelector("#observed2");
 });
 
-it("should render with 1x1 initially, before the ResizeObserver is triggered", async () => {
-  expect(observed.textContent).toBe("1x1");
+it("hould render with 1x1 initially, before the ResizeObserver is triggered", async () => {
+  expect(observed1.textContent).toBe("1 1x1");
+  expect(observed2.textContent).toBe("2 1x1");
 });
 
 it("should report the correct size after the size is reported by the ResizeObserver", async () => {
   await delay(100);
-
-  expect(observed.textContent).toBe("200x300");
+  expect(observed1.textContent).toBe("1 200x300");
+  expect(observed2.textContent).toBe("2 200x300");
 });
 
 it("should report following size changes", async () => {
@@ -59,5 +66,6 @@ it("should report following size changes", async () => {
   app.style.height = "100px";
 
   await delay(100);
-  expect(observed.textContent).toBe("100x100");
+  expect(observed1.textContent).toBe("1 100x100");
+  expect(observed2.textContent).toBe("2 100x100");
 });
