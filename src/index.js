3cf8ada579f4485fce5e9208f66189715dd04ead
Михайлов Антон <anion155@gmail.com>
2019-06-19 16:49:55 +0500

    Allow passing reference object to hook.
    It is essential in cases, when ref must be created in some other place, than this hook. Updated example of usage https://codesandbox.io/s/useresizeobserver-react-hook-9t1g8?fontsize=14

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
 import { useEffect, useState, useRef } from "react";
 import ResizeObserver from "resize-observer-polyfill";
 
-export default function() {
-  const ref = useRef();
+export default function(ref) {
+  const defaultRef = useRef();
+  ref = ref || defaultRef;
+  const [observer, setObserver] = useState(null);
   const [width, changeWidth] = useState(1);
   const [height, changeHeight] = useState(1);
-
+  
   useEffect(() => {
-    const element = ref.current;
     const resizeObserver = new ResizeObserver(entries => {
-      if (!Array.isArray(entries)) {
-        return;
-      }
-
       // Since we only observe the one element, we don't need to loop over the
       // array
-      if (!entries.length) {
+      if (!Array.isArray(entries) || !entries.length) {
         return;
       }
-
       const entry = entries[0];
-
       changeWidth(entry.contentRect.width);
       changeHeight(entry.contentRect.height);
     });
-
-    resizeObserver.observe(element);
-
-    return () => resizeObserver.unobserve(element);
+    setObserver(resizeObserver);
+    return () => resizeObserver.disconnect();
   }, []);
 
-  return [ref, width, height];
+  useEffect(() => {
+    const element = ref.current;
+    if (!observer || !element) {
+      return;
+    }
+    observer.observe(element);
+    return () => observer.unobserve(element);
+  }, [observer, ref]);
+
+  // Since ref can be passed to function manually. It must be optional output.
+  // Save compatibility with v3.1.0 and earlier.
+  if (defaultRef === ref) {
+    return [ref, width, height];
+  } else {
+    return [width, height, ref];
+  }
 }
