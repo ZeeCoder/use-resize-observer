a3754325ee7f3476d28b0a45a245c1ae190af4d9
Viktor Hubert <rpgmorpheus@gmail.com>
2019-11-18 21:22:18 +0100

    Transpiling again, separate polyfilled build.
    Resolves #14

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,41 @@
-export default {
-  input: "src/index.js",
-  output: [
-    {
-      file: "dist/bundle.cjs.js",
-      format: "cjs"
-    },
-    {
-      file: "dist/bundle.esm.js",
-      format: "esm"
-    }
-  ],
-  external: ["react"]
+import babel from "rollup-plugin-babel";
+import inject from "rollup-plugin-inject";
+
+const getConfig = polyfill => {
+  const config = {
+    input: "src/index.js",
+    output: [],
+    plugins: [babel()],
+    external: ["react"]
+  };
+
+  if (polyfill) {
+    config.output = [
+      {
+        file: "polyfilled.js",
+        format: "cjs"
+      }
+    ];
+    config.external.push("resize-observer-polyfill");
+    config.plugins.push(
+      inject({
+        ResizeObserver: "resize-observer-polyfill"
+      })
+    );
+  } else {
+    config.output = [
+      {
+        file: "dist/bundle.cjs.js",
+        format: "cjs"
+      },
+      {
+        file: "dist/bundle.esm.js",
+        format: "esm"
+      }
+    ];
+  }
+
+  return config;
 };
+
+export default [getConfig(), getConfig(true)];
